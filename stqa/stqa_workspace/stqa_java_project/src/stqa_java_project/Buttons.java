ea1533e7680134063b0e72bc17e576b33ec6de8c
Atman-Shastri <atmanshastri@gmail.com>
2023-10-11 16:30:40 +0530

    Wed 10/11/2023 04:30 PM

--- stqa/stqa_workspace/stqa_java_project/src/stqa_java_project/Buttons.java
+++ stqa/stqa_workspace/stqa_java_project/src/stqa_java_project/Buttons.java
@@ -4,19 +4,20 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class Locators {
+public class Buttons {
 	
 	public static void main(String[] args) {
 		LaunchBrowsers browser = new LaunchBrowsers();
 		String chromeDriverPath = "../../libs_and_drivers/chromedriver.exe";
 		
-		String url1 = "http://demo.guru99.com/test/facebook.html";
+		String url1 = "https://demo.guru99.com/test/radio.html";
 		
 		WebDriver chrome = browser.initializeChrome(chromeDriverPath);
 		chrome.get(url1);
-		chrome.findElement(By.name("email")).sendKeys("Atman");
-		chrome.findElement(By.xpath("//*[@id=\"pass\"]")).sendKeys("Password"); 
-		chrome.findElement(By.xpath("//*[@id=\"u_0_b\"]")).click();
+		chrome.findElement(By.id("vfb-7-3")).click(); // Radio Button (Option 2)
+		chrome.findElement(By.id("vfb-6-2")).click(); // Checkbox 2
+		chrome.findElement(By.id("vfb-6-1")).click(); // CheckBox 3
+		
 	}
 
 }
--- stqa/stqa_workspace/stqa_java_project/src/stqa_java_project/HTMLActions.java
+++ stqa/stqa_workspace/stqa_java_project/src/stqa_java_project/HTMLActions.java
@@ -1,22 +1,29 @@
 package stqa_java_project;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class Locators {
+
+public class HTMLActions {
 	
 	public static void main(String[] args) {
 		LaunchBrowsers browser = new LaunchBrowsers();
 		String chromeDriverPath = "../../libs_and_drivers/chromedriver.exe";
 		
-		String url1 = "http://demo.guru99.com/test/facebook.html";
+		String url1 = "https://demo.guru99.com/test/simple_context_menu.html";
 		
 		WebDriver chrome = browser.initializeChrome(chromeDriverPath);
 		chrome.get(url1);
-		chrome.findElement(By.name("email")).sendKeys("Atman");
-		chrome.findElement(By.xpath("//*[@id=\"pass\"]")).sendKeys("Password"); 
-		chrome.findElement(By.xpath("//*[@id=\"u_0_b\"]")).click();
+		chrome.manage().window().maximize();
+		Actions action = new Actions(chrome);
+		
+		WebElement link = chrome.findElement(By.xpath("//*[@id=\"authentication\"]/button"));
+		
+		action.doubleClick(link).perform();
+		
 	}
 
 }
