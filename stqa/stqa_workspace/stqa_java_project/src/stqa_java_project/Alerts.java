54fcc37211da056e52d120d24ea20f5fbd29bbd8
Atman-Shastri <atmanshastri@gmail.com>
2023-10-18 15:41:22 +0530

    Wed 10/18/2023 03:41 PM

---
+++ stqa/stqa_workspace/stqa_java_project/src/stqa_java_project/Alerts.java
@@ -0,0 +1,34 @@
+package stqa_java_project;
+
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class Alerts {
+
+	public static void main(String[] args) {
+		LaunchBrowsers browser = new LaunchBrowsers();
+		String chromeDriverPath = "../../libs_and_drivers/chromedriver.exe";
+		WebDriver chrome = browser.initializeChrome(chromeDriverPath);
+		
+		String url = "https://demo.guru99.com/test/delete_customer.php";
+		chrome.get(url);
+		
+		chrome.findElement(By.name("cusid")).sendKeys("Atman");
+		chrome.findElement(By.name("submit")).click();
+		
+		Alert alert = chrome.switchTo().alert();
+		String alertText = alert.getText();
+		System.out.println("Alert is: " + alertText);
+		
+		alert.accept();
+		String newAlertText = alert.getText();
+		System.out.println("New Alert is: " + newAlertText);
+		alert.accept();
+		
+	
+		
+
+	}
+
+}
