cc086e48bf8f8d642baaaf9983b85d076aab09ae
Atman-Shastri <atmanshastri@gmail.com>
2023-10-30 12:20:23 +0530

    30-10-2023 12:20

--- ds_cc/client_workspace/practical_three/src/practicalfour/MyServer.java
+++ ds_cc/client_workspace/practical_three/src/practicalfour/MyServer.java
@@ -21,16 +21,21 @@ public class MyServer extends UnicastRemoteObject implements MyInterface {
 	@Override
 	public String getData() throws Exception {
 		try {
-			Class.forName("com.mysql.jdbc.Driver");
-			Connection connection = DriverManager.getConnection("jdbc:mysql://localhost/Library","root","root");
+			Class.forName("com.mysql.cj.jdbc.Driver");
+//			Connection connection = DriverManager.getConnection("jdbc:mysql://localhost/Library","root","root");
+			Connection connection = DriverManager.getConnection("jdbc:mysql://localhost/electric_bill","root","root");
 			java.sql.Statement statement = connection.createStatement();
 			
-			String query = "Select * from book";
+			String query = "Select * from bill";
 			ResultSet rs = statement.executeQuery(query);
 			
 			while(rs.next()) {
-				str += rs.getInt(1) + " " + rs.getString(2) + " " + rs.getString(3) + "\n";
+				str += rs.getString(1) + " " + rs.getString(2) + " " + rs.getFloat(3) + "\n";
 			}
+			
+//			while(rs.next()) {
+//				str += rs.getInt(1) + " " + rs.getString(2) + " " + rs.getString(3) + "\n";
+//			}
 			
 		} catch(Exception exception) {
 			System.out.println(exception);
