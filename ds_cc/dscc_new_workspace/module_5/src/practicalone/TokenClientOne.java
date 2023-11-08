f4fd78a767efef603b59bda464a6d220e561f568
Atman-Shastri <atmanshastri@gmail.com>
2023-11-08 15:02:09 +0530

    Wed 11/08/2023 03:02 PM

---
+++ ds_cc/dscc_new_workspace/module_5/src/practicalone/TokenClientOne.java
@@ -0,0 +1,55 @@
+package practicalone;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+
+
+public class TokenClientOne {
+	public static DatagramSocket ds;
+	public static DatagramPacket dp;
+	public static BufferedReader br;
+
+	public static void main(String[] args) throws Exception {
+		boolean hasToken = false;
+		ds = new DatagramSocket(100);
+		while (true) {
+			if (hasToken == true) {
+				System.out.println("Do you want to write data...(yes/no)");
+				br = new BufferedReader(new InputStreamReader(System.in));
+				String ans = br.readLine();
+				if (ans.equalsIgnoreCase("yes")) {
+					System.out.println("ready to write");
+					System.out.println("enter the data");
+					br = new BufferedReader(new InputStreamReader(System.in));
+					String str = "Client-1===>" + br.readLine();
+					byte buff[] = new byte[1024];
+					buff = str.getBytes();
+					ds.send(new DatagramPacket(buff, buff.length, InetAddress.getLocalHost(), 1000));
+					System.out.println("now sending");
+				} else if (ans.equalsIgnoreCase("no")) {
+					System.out.println("I am Busy state");
+					String msg = "token";
+					byte bf1[] = new byte[1024];
+					bf1 = msg.getBytes();
+					ds.send(new DatagramPacket(bf1, bf1.length, InetAddress.getLocalHost(), 200));
+					hasToken = false;
+				}
+			} else {
+				System.out.println("Entering in receiving mode");
+				byte bf[] = new byte[1024];
+				ds.receive(dp = new DatagramPacket(bf, bf.length));
+				String clientmsg = new String(dp.getData(), 0, dp.getLength());
+				System.out.println("The data is " + clientmsg);
+				if (clientmsg.equals("token")) {
+					hasToken = true;
+					System.out.println("I am leaving busy state");
+				}
+			}
+		}
+
+	}
+
+}
---
+++ ds_cc/dscc_new_workspace/module_5/src/practicalone/TokenServer.java
@@ -0,0 +1,21 @@
+package practicalone;
+
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.SocketException;
+
+public class TokenServer {
+
+	public static DatagramSocket ds;
+	public static DatagramPacket dp;
+
+	public static void main(String[] args) throws Exception {
+		ds = new DatagramSocket(1000);
+		while (true) {
+			byte buff[] = new byte[1024];
+			ds.receive(dp = new DatagramPacket(buff, buff.length));
+			String str = new String(dp.getData(), 0, dp.getLength());
+			System.out.println("Message from " + str);
+		}
+	}
+}
